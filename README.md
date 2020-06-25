b02ac497cf826d40b807368ff93c7b5cb46c9565
trapholov <trapholov@gmail.com>
2020-06-25 03:03:53 +0300

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+```
+# To run the app:
+flutter run
+
+# To run the app showing Arabic:
+flutter run lib/arabic.dart
+
+# Release:
+flutter clean && flutter build appbundle --release
+```
---
+++ lib/main.dart
@@ -0,0 +1,133 @@
+import 'package:video_player/video_player.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'src/generator.dart';
+
+void main() {
+  runApp(VideoApp());
+}
+
+class VideoApp extends StatefulWidget {
+  @override
+  _VideoAppState createState() => _VideoAppState();
+}
+
+class _VideoAppState extends State<VideoApp> {
+  VideoPlayerController _controller;
+  String person = '';
+  String action = '';
+  String conclusion = '';
+
+  @override
+  void initState() {
+    var madness = new MadNews();
+    this.person = madness.getPerson();
+    this.action = madness.getAction();
+    this.conclusion = madness.getConclusion();
+    super.initState();
+    _controller = VideoPlayerController.asset("assets/shower.mp4")
+      ..initialize().then((_) {
+        _controller.play();
+        _controller.setLooping(true);
+        // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
+        setState(() {});
+      });
+  }
+
+  void reloadMadness() {
+    var madness = new MadNews();
+    this.person = madness.getPerson();
+    this.action = madness.getAction();
+    this.conclusion = madness.getConclusion();
+    setState(() {});
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MadNews',
+      home: SafeArea(
+        child: Scaffold(
+          body: GestureDetector(
+            onTap: () {
+              this.reloadMadness();
+            },
+            child: Stack(children: <Widget>[
+              SizedBox.expand(
+                child: FittedBox(
+                  fit: BoxFit.fitHeight,
+                  alignment: Alignment.bottomLeft,
+                  child: SizedBox(
+                    width: _controller.value.size?.width ?? 0,
+                    height: _controller.value.size?.height ?? 0,
+                    child: _controller.value.initialized
+                        ? AspectRatio(
+                            aspectRatio: _controller.value.aspectRatio,
+                            child: VideoPlayer(_controller),
+                          )
+                        : Container(),
+                  ),
+                ),
+              ),
+              FittedBox(
+                fit: BoxFit.fitHeight,
+                alignment: Alignment.bottomLeft,
+                child: SizedBox(
+                  width: 300,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.only(
+                            top: 40, bottom: 10, left: 20, right: 20),
+                        child: Text(
+                          '${this.person}',
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.w800,
+                            color: Colors.white,
+                            backgroundColor: Colors.black87,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(bottom: 10, left: 20, right: 20),
+                        child: Text(
+                          '${this.action}',
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.w800,
+                            color: Colors.white,
+                            backgroundColor: Colors.black87,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(bottom: 20, left: 20, right: 20),
+                        child: Text(
+                          '${this.conclusion}',
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.w800,
+                            color: Colors.white,
+                            backgroundColor: Colors.black87,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ]),
+          ),
+        ),
+      ),
+    );
+  }
+}
